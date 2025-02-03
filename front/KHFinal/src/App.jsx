1757750f399b826bdcb07745e7ba0ba56ff81815
mskhim <mskhim1@naver.com>
2025-02-03 18:32:55 +0900

    [수정] 관리자 컴포넌트 라우터 분리

--- front/KHFinal/src/App.jsx
+++ front/KHFinal/src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import RouterComponent from './RouterComponent';
+import RouterComponentAdmin from './RouterComponentAdmin';
+
 import { Provider } from './Context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
