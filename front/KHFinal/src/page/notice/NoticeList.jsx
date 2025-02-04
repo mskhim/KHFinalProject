7d6e9a1df80e673e527ca491472dfdc873e3ddfe
ostarry <ostarry@gmail.com>
2025-02-04 17:50:58 +0900

    [생성] 공지사항 메인 UI 버튼 생성 및 공지사항 상세보기 기능추가

--- front/KHFinal/src/page/notice/NoticeList.jsx
+++ front/KHFinal/src/page/notice/NoticeList.jsx
@@ -1,24 +1,28 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import "./notice.css";
+import "./Notice.css";
+import { Link } from "react-router-dom";
+import { Context } from "../../Context";
+import { useContext } from "react";
 
 const NoticeList = () => {
+  const { getDarkMode } = useContext(Context);
   return (
     <>
       <Header />
-      <section className="notice">
-        <div className="page-title">
-          <div className="container">
+      <section className="Notice-notice">
+        <div className="Notice-page-title">
+          <div className="Notice-container">
             <h3>공지사항</h3>
           </div>
         </div>
 
-        <div id="board-search">
-          <div className="container">
-            <div className="search-window">
+        <div className="Notice-board-search">
+          <div className="Notice-container">
+            <div className="Notice-search-window">
               <form action="">
-                <div className="search-wrap">
-                  <label htmlFor="search" className="blind">
+                <div className="Notice-search-wrap">
+                  <label htmlFor="search" className="Notice-blind">
                     공지사항 내용 검색
                   </label>
                   <input
@@ -26,7 +30,11 @@ const NoticeList = () => {
                     type="search"
                     placeholder="검색어를 입력해주세요."
                   />
-                  <button type="submit" className="btn btn-dark">
+                  <button
+                    onClick
+                    type="submit"
+                    className="Notice-btn Notice-btn-dark"
+                  >
                     검색
                   </button>
                 </div>
@@ -35,27 +43,29 @@ const NoticeList = () => {
           </div>
         </div>
 
-        <div id="board-list">
-          <div className="container">
-            <table className="board-table">
+        <div className="Notice-board-list">
+          <div className="Notice-container">
+            <table className="Notice-board-table">
               <thead>
                 <tr>
-                  <th scope="col" className="th-num">
+                  <th scope="col" className="Notice-th-num">
                     번호
                   </th>
-                  <th scope="col" className="th-title">
+                  <th scope="col" className="Notice-th-title">
                     제목
                   </th>
-                  <th scope="col" className="th-date">
+                  <th scope="col" className="Notice-th-date">
                     등록일
                   </th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>3</td>
+                  <td>1</td>
                   <th>
-                    <a href="#!">[공지사항] 개인정보 처리방침 변경안내</a>
+                    <Link to="/noticeRead/1" className={getDarkMode()}>
+                      [공지사항] 개인정보 처리방침 변경안내
+                    </Link>
                     <p>테스트</p>
                   </th>
                   <td>2017.07.13</td>
@@ -63,23 +73,28 @@ const NoticeList = () => {
                 <tr>
                   <td>2</td>
                   <th>
-                    <a href="#!">
+                    <Link to="" className={getDarkMode()}>
                       공지사항 안내입니다. 이용해주셔서 감사합니다
-                    </a>
+                    </Link>
                   </th>
                   <td>2017.06.15</td>
                 </tr>
                 <tr>
-                  <td>1</td>
+                  <td>3</td>
                   <th>
-                    <a href="#!">
+                    <Link to="" className={getDarkMode()}>
                       공지사항 안내입니다. 이용해주셔서 감사합니다
-                    </a>
+                    </Link>
                   </th>
                   <td>2017.06.15</td>
                 </tr>
               </tbody>
             </table>
+          </div>
+          <div className="Notice-main-btn">
+            <button className="Notice-main-btn Notice-main-btn-dark">
+              글쓰기
+            </button>
           </div>
         </div>
       </section>
--- front/KHFinal/src/page/notice/NoticeRead.jsx
+++ front/KHFinal/src/page/notice/NoticeRead.jsx
@@ -1,11 +1,64 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "./noticeRead.css";
+import { Link } from "react-router-dom";
 
 const NoticeRead = () => {
   return (
     <>
       <Header />
-      <h1>NoticeRead</h1>
+      <section className="NoticeRead-notice">
+        <div className="NoticeRead-page-title">
+          <div className="NoticeRead-container">
+            <h3>
+              <Link to="/NoticeList">공지사항</Link>
+            </h3>
+          </div>
+        </div>
+
+        <div className="NoticeRead-board-view">
+          <div className="NoticeRead-container">
+            {/* 목록으로 버튼 */}
+            <div className="NoticeRead-btn-wrap">
+              <button className="NoticeRead-btn Notice-btn-dark">
+                목록으로
+              </button>
+            </div>
+
+            {/* 게시글 테이블 */}
+            <table className="NoticeRead-board-table">
+              <tbody>
+                <tr>
+                  <td colSpan="2" className="NoticeRead-view-title">
+                    [공지사항] 개인정보 처리방침 변경안내
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2" className="NoticeRead-view-content">
+                    <p>
+                      안녕하세요. 개인정보 처리방침이 변경되었습니다. 새로운
+                      정책을 확인하시고 이용해주시기 바랍니다.
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            {/* 등록일 및 수정/삭제 버튼 */}
+            <div className="NoticeRead-meta">
+              <span className="NoticeRead-date">등록일: 2025-02-05</span>
+              <div className="NoticeRead-btn-group">
+                <button className="NoticeRead-btn NoticeRead-btn-light">
+                  수정
+                </button>
+                <button className="NoticeRead-btn NoticeRead-btn-dark">
+                  삭제
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
