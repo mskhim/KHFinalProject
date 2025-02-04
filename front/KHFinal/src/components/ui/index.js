1d6d7977377b93ae8d9ddf644aae72f52c556afd
mskhim <mskhim1@naver.com>
2025-02-04 15:15:06 +0900

    [수정] 버튼 컴포넌트 디테일 추가

--- front/KHFinal/src/components/ui/index.js
+++ front/KHFinal/src/components/ui/index.js
@@ -1,3 +1,3 @@
 export { default as ButtonDarkMode } from './ButtonDarkMode';
 export { default as ButtonRole } from './ButtonRole';
-export { default as ButtonUser } from './ButtonRole';
+export { default as ButtonNickName } from './ButtonNickName';
