40cf51b43ade247a4f6515daf2c523c0f4dcd882
mskhim <mskhim1@naver.com>
2025-02-07 15:41:05 +0900

    [수정]  헤더에 sticky, 디테일추가

--- front/KHFinal/src/components/Header.jsx
+++ front/KHFinal/src/components/Header.jsx
@@ -108,235 +108,239 @@ const Header = ({ page }) => {
   return (
     <>
       <ScrollToTopButton />
-      <Navbar
-        expand="lg"
-        className={`py-3 ${
-          darkMode ? 'bg-dark text-light' : 'bg-white text-dark'
-        } `}
-      >
-        <Container
-          fluid
-          className="Header-header-container px-4"
-          id="Header-headerContainer"
+      <div className="Header-top-line position-sticky top-0 w-100">
+        <Navbar
+          expand="lg"
+          className={`py-3 ${
+            darkMode ? 'bg-dark text-light' : 'bg-white text-dark'
+          } Header-navbar-all${showFestivalNav ? '-active' : ''}`} // ✅ 축제 네비게이션 활성화 여부에 따라 클래스 추가
         >
-          <Navbar.Brand
-            as={NavLink}
-            to="/"
-            className={`${darkMode ? 'text-light' : 'text-dark'} `}
+          <Container
+            fluid
+            className="Header-header-container px-4"
+            id="Header-headerContainer"
           >
-            <img
-              src={ViVaFesta}
-              alt="ViVaFesta"
-              className="Header-logo align-content-center pb-1"
-              height={20}
-            />
-          </Navbar.Brand>
-
-          <Navbar.Toggle aria-controls="navbarColor01" />
-          <Navbar.Collapse id="navbarColor01">
-            <Nav className="me-auto Header-nav-links">
-              <Nav.Link
-                className={darkMode ? 'text-light' : 'text-dark'}
-                onClick={!page ? toggleFestivalNav : undefined}
-              >
-                축제
-              </Nav.Link>
-              <NavDropdown
-                title={
-                  <span className={`${darkMode ? 'text-light' : 'text-dark'}`}>
-                    고객지원{' '}
-                    <BsChevronDown
-                      size={16}
-                      className={`ms-1 Header-dropdown-icon ${
-                        darkMode ? 'text-light' : 'text-dark'
-                      }`}
-                    />
-                  </span>
-                }
-                id="Header-user-dropdown"
-                align="end"
-                className="Header-user-dropdown"
-                menuVariant={darkMode ? 'dark' : 'light'} // ✅ 다크 모드에 따라 메뉴 스타일 변경
-              >
-                <NavDropdown.Item
-                  as={NavLink}
-                  to="/qnaList"
-                  className={darkMode ? 'text-light' : 'text-dark'} // ✅ 다크 모드 적용
-                >
-                  QNA
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  as={NavLink}
-                  to="/noticeList"
-                  className={darkMode ? 'text-light' : 'text-dark'} // ✅ 다크 모드 적용
-                >
-                  공지사항
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link
-                as={NavLink}
-                to="/manager/managerStats"
-                className={`${darkMode ? 'text-light' : 'text-dark'}`}
-              >
-                통계
-              </Nav.Link>
-            </Nav>
-
-            <div className="d-flex align-items-center">
-              {/* {isAuthenticated ? ( */}
-
-              <NavDropdown
-                title={
-                  <span className="d-flex align-items-center Header-user-info">
-                    <BsPersonCircle
-                      size={24}
-                      className={`me-2 ${
-                        darkMode ? 'text-light' : 'text-dark'
-                      }`}
-                    />
-                    <span
-                      className={`Header-user-name ${
-                        darkMode ? 'text-light' : 'text-dark'
-                      }`}
-                    >
-                      {userName}
-                    </span>
-                    <BsChevronDown
-                      size={16}
-                      className={`ms-1 Header-dropdown-icon ${
-                        darkMode ? 'text-light' : 'text-dark'
-                      }`}
-                    />
-                  </span>
-                }
-                id="Header-user-dropdown"
-                align="end"
-                className="Header-user-dropdown"
-              >
-                <NavDropdown.Item as={NavLink} to="/user/userMypage">
-                  마이페이지
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/user/bookingList">
-                  예매 내역 확인
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item onClick={Logout}>로그아웃</NavDropdown.Item>
-              </NavDropdown>
-              <TokenRemain
-                initialExpiration={tokenExpiration}
-                onExtend={handleExtendToken}
+            <Navbar.Brand
+              as={NavLink}
+              to="/"
+              className={`${darkMode ? 'text-light' : 'text-dark'} `}
+            >
+              <img
+                src={ViVaFesta}
+                alt="ViVaFesta"
+                className="Header-logo align-content-center pb-1"
+                height={20}
               />
-              {/* ) : ( */}
-              <Button
-                variant={darkMode ? 'outline-light' : 'outline-dark'}
-                onClick={handleLogin}
-                className="me-3"
-              >
-                로그인
-              </Button>
-              {/* )} */}
-              {isAuthenticated && (
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="navbarColor01" />
+            <Navbar.Collapse id="navbarColor01">
+              <Nav className="me-auto Header-nav-links">
+                <Nav.Link
+                  className={darkMode ? 'text-light' : 'text-dark'}
+                  onClick={!page ? toggleFestivalNav : undefined}
+                >
+                  축제
+                </Nav.Link>
+                <NavDropdown
+                  title={
+                    <span
+                      className={`${darkMode ? 'text-light' : 'text-dark'}`}
+                    >
+                      고객지원{' '}
+                      <BsChevronDown
+                        size={16}
+                        className={`ms-1 Header-dropdown-icon ${
+                          darkMode ? 'text-light' : 'text-dark'
+                        }`}
+                      />
+                    </span>
+                  }
+                  id="Header-user-dropdown"
+                  align="end"
+                  className="Header-user-dropdown"
+                  menuVariant={darkMode ? 'dark' : 'light'} // ✅ 다크 모드에 따라 메뉴 스타일 변경
+                >
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/qnaList"
+                    className={darkMode ? 'text-light' : 'text-dark'} // ✅ 다크 모드 적용
+                  >
+                    QNA
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/noticeList"
+                    className={darkMode ? 'text-light' : 'text-dark'} // ✅ 다크 모드 적용
+                  >
+                    공지사항
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link
+                  as={NavLink}
+                  to="/manager/managerStats"
+                  className={`${darkMode ? 'text-light' : 'text-dark'}`}
+                >
+                  통계
+                </Nav.Link>
+              </Nav>
+
+              <div className="d-flex align-items-center">
+                {/* {isAuthenticated ? ( */}
+
+                <NavDropdown
+                  title={
+                    <span className="d-flex align-items-center Header-user-info">
+                      <BsPersonCircle
+                        size={24}
+                        className={`me-2 ${
+                          darkMode ? 'text-light' : 'text-dark'
+                        }`}
+                      />
+                      <span
+                        className={`Header-user-name ${
+                          darkMode ? 'text-light' : 'text-dark'
+                        }`}
+                      >
+                        {userName}
+                      </span>
+                      <BsChevronDown
+                        size={16}
+                        className={`ms-1 Header-dropdown-icon ${
+                          darkMode ? 'text-light' : 'text-dark'
+                        }`}
+                      />
+                    </span>
+                  }
+                  id="Header-user-dropdown"
+                  align="end"
+                  className="Header-user-dropdown"
+                >
+                  <NavDropdown.Item as={NavLink} to="/user/userMypage">
+                    마이페이지
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/user/bookingList">
+                    예매 내역 확인
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={Logout}>로그아웃</NavDropdown.Item>
+                </NavDropdown>
+                <TokenRemain
+                  initialExpiration={tokenExpiration}
+                  onExtend={handleExtendToken}
+                />
+                {/* ) : ( */}
+                <Button
+                  variant={darkMode ? 'outline-light' : 'outline-dark'}
+                  onClick={handleLogin}
+                  className="me-3"
+                >
+                  로그인
+                </Button>
+                {/* )} */}
+                {isAuthenticated && (
+                  <div className="position-relative me-3">
+                    <FaBell
+                      size={24}
+                      className={`${
+                        darkMode ? 'text-light' : 'text-dark'
+                      } Header-cart-icon`}
+                    />
+                    {notifications > 0 && (
+                      <Badge
+                        pill
+                        bg="danger"
+                        className="Header-notification-badge Header-cart-icon"
+                      >
+                        {notifications}
+                      </Badge>
+                    )}
+                  </div>
+                )}
+                {/* {isAuthenticated && ( */}
                 <div className="position-relative me-3">
-                  <FaBell
+                  <FaCartPlus
                     size={24}
-                    className={`${
-                      darkMode ? 'text-light' : 'text-dark'
-                    } Header-cart-icon`}
+                    className={`${getDarkMode} ms-3 Header-cart-icon`}
+                    onClick={() => navigate('/userCart')}
                   />
                   {notifications > 0 && (
                     <Badge
                       pill
-                      bg="danger"
+                      bg="success"
                       className="Header-notification-badge Header-cart-icon"
+                      onClick={() => navigate('/user/userCart')}
                     >
-                      {notifications}
+                      {cartElement}
                     </Badge>
                   )}
                 </div>
-              )}
-              {/* {isAuthenticated && ( */}
-              <div className="position-relative me-3">
-                <FaCartPlus
-                  size={24}
-                  className={`${getDarkMode} ms-3 Header-cart-icon`}
-                  onClick={() => navigate('/userCart')}
-                />
-                {notifications > 0 && (
-                  <Badge
-                    pill
-                    bg="success"
-                    className="Header-notification-badge Header-cart-icon"
-                    onClick={() => navigate('/user/userCart')}
-                  >
-                    {cartElement}
-                  </Badge>
-                )}
+                {/* )} */}
+                <a
+                  href="https://github.com/your-github-profile"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="Header-github-link ms-3"
+                >
+                  <FaGithub
+                    size={30}
+                    className={darkMode ? 'text-light' : 'text-dark'}
+                  />
+                </a>
+                <Button
+                  variant="link"
+                  className="Header-theme-toggle-btn me-3"
+                  onClick={toggleDarkMode}
+                >
+                  {darkMode ? (
+                    <FaSun size={24} className="text-light" />
+                  ) : (
+                    <FaMoon size={24} className="text-dark" />
+                  )}
+                </Button>
               </div>
-              {/* )} */}
-              <a
-                href="https://github.com/your-github-profile"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="Header-github-link ms-3"
-              >
-                <FaGithub
-                  size={30}
-                  className={darkMode ? 'text-light' : 'text-dark'}
-                />
-              </a>
-              <Button
-                variant="link"
-                className="Header-theme-toggle-btn me-3"
-                onClick={toggleDarkMode}
-              >
-                {darkMode ? (
-                  <FaSun size={24} className="text-light" />
-                ) : (
-                  <FaMoon size={24} className="text-dark" />
-                )}
-              </Button>
-            </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <div
-        className={`Header-festival-nav ${
-          darkMode ? 'bg-dark text-light' : 'bg-white text-dark'
-        } ${
-          showFestivalNav
-            ? isAnimating
-              ? 'Header-with-animation'
-              : 'visible'
-            : 'Header-hidden'
-        }`}
-      >
-        <Container fluid className="Header-festival-nav-container ms-5">
-          <div
-            className={`Header-festival-nav-item ${
-              activeTab === 'list' ? 'Header-active' : ''
-            } `}
-            onClick={() => navigate('/eventList')}
-          >
-            <span>축제 목록</span>
-          </div>
-          <div
-            className={`Header-festival-nav-item ${
-              activeTab === 'cal' ? 'Header-active' : ''
-            }`}
-            onClick={() => navigate('/eventCalendar')}
-          >
-            <span>축제 달력</span>
-          </div>
-          <div
-            className={`Header-festival-nav-item ${
-              activeTab === 'map' ? 'Header-active' : ''
-            } `}
-            onClick={() => navigate('/eventMap')}
-          >
-            <span>지역별 축제</span>
-          </div>
-        </Container>
+        <div
+          className={`Header-festival-nav ${
+            darkMode ? 'bg-dark text-light' : 'bg-white text-dark'
+          } ${
+            showFestivalNav
+              ? isAnimating
+                ? 'Header-with-animation'
+                : 'visible'
+              : 'Header-hidden'
+          }`}
+        >
+          <Container fluid className="Header-festival-nav-container ms-5">
+            <div
+              className={`Header-festival-nav-item ${
+                activeTab === 'list' ? 'Header-active' : ''
+              } `}
+              onClick={() => navigate('/eventList')}
+            >
+              <span>축제 목록</span>
+            </div>
+            <div
+              className={`Header-festival-nav-item ${
+                activeTab === 'cal' ? 'Header-active' : ''
+              }`}
+              onClick={() => navigate('/eventCalendar')}
+            >
+              <span>축제 달력</span>
+            </div>
+            <div
+              className={`Header-festival-nav-item ${
+                activeTab === 'map' ? 'Header-active' : ''
+              } `}
+              onClick={() => navigate('/eventMap')}
+            >
+              <span>지역별 축제</span>
+            </div>
+          </Container>
+        </div>
       </div>
     </>
   );
