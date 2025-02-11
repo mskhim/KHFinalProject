a6fa2483cd39c77d4cf379725c535cb7229a8046
mskhim <mskhim1@naver.com>
2025-02-11 13:42:56 +0900

    [수정] 업로드경로 깃 이그노어 설정

---
+++ back/KHFinalProject/src/main/java/com/zeus/event/domain/EventDTO.java
@@ -0,0 +1,16 @@
+package com.zeus.event.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class EventDTO {
+    private int no;               // 시퀀스 (PK)
+    private int publicDataEventNo; // 공공데이터 축제번호 (FK)
+    private int userAccountNo;     // 작성자(매니저) (FK)
+    private int price;         // 이용권 가격
+    private Double rating;         // 평점
+    private String thumbUrl; // 메인 이미지 URL (유니크)
+}
--- back/KHFinalProject/src/main/java/com/zeus/event/mapper/EventMapper.java
+++ back/KHFinalProject/src/main/java/com/zeus/event/mapper/EventMapper.java
@@ -1,5 +1,19 @@
 package com.zeus.event.mapper;
 
-public interface EventMapper {
+import java.util.List;
 
+import com.zeus.event.domain.Event;
+import com.zeus.event.domain.EventImg;
+
+public interface EventMapper {
+	//이벤트 게시판에 등록
+	boolean insertEvent(Event event);
+	//이벤트 게시판 no 값을 fk로 타이틀, 서브이미지 등록
+	boolean insertEventImg(EventImg eventImg);
+	//이벤트 리스트 출력 기본출력은 그냥 등록순으로 할것 page값으로 페이징 처리.
+	List<Event> selectEventList(String page);
+	//이벤트 리스트 출력 sort할 항목을 찾아서 sorting page값으로 페이징 처리.
+	boolean selectEventListSorted(String page);
+	
+	
 }
